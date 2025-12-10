30eaa853fab0c3510fb43a8990790e106ab6bb4b
ImplFerris <implferris@proton.me>
2025-12-10 07:29:14 +0530

    writing single byte image into oled

---
+++ blocking/byte-oled/src/main.rs
@@ -0,0 +1,88 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_rp as hal;
+use embassy_rp::block::ImageDef;
+use embassy_time::Timer;
+
+//Panic Handler
+use panic_probe as _;
+// Defmt Logging
+use defmt_rtt as _;
+
+// I2C
+use embassy_rp::i2c::{self, Config};
+
+// OLED
+use ssd1306::{I2CDisplayInterface, Ssd1306, prelude::*};
+
+// Embedded Graphics
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    pixelcolor::BinaryColor,
+    prelude::Point,
+    prelude::*,
+};
+
+/// Tell the Boot ROM about our application
+#[unsafe(link_section = ".start_block")]
+#[used]
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+
+// 8x5 pixels
+#[rustfmt::skip]
+const IMG_DATA: &[u8] = &[
+    0b00111000,
+    0b01000100,
+    0b01000100,
+    0b00101000,
+    0b11101110,
+];
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let sda = p.PIN_18;
+    let scl = p.PIN_19;
+
+    let mut i2c_config = Config::default();
+    i2c_config.frequency = 400_000; //400kHz
+
+    let i2c = i2c::I2c::new_blocking(p.I2C1, scl, sda, i2c_config);
+
+    let interface = I2CDisplayInterface::new(i2c);
+
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+
+    display.init().expect("failed to initialize the display");
+
+    let raw_image = ImageRaw::<BinaryColor>::new(IMG_DATA, 8);
+
+    let image = Image::new(&raw_image, Point::zero());
+
+    image
+        .draw(&mut display)
+        .expect("failed to draw text to display");
+
+    display.flush().expect("failed to flush data to display");
+
+    loop {
+        Timer::after_millis(100).await;
+    }
+}
+
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recomended to have these minimal entries.
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"byte-oled"),
+    embassy_rp::binary_info::rp_program_description!(c"your program description"),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
+
+// End of file
