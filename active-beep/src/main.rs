fc50f1b39d4182f38c6adfc47e9ccadc0221fda3
ImplFerris <implferris@proton.me>
2025-12-24 23:29:29 +0530

    active beep updated

--- active-beep/src/main.rs
+++ active-beep/src/main.rs
@@ -1,24 +1,28 @@
 #![no_std]
 #![no_main]
 
-use embassy_rp as hal;
 use embassy_executor::Spawner;
+use embassy_rp as hal;
 use embassy_rp::block::ImageDef;
-use embassy_rp::gpio;
 use embassy_time::Timer;
-use gpio::{Level, Output};
-use {defmt_rtt as _, panic_probe as _};
 
+//Panic Handler
+use panic_probe as _;
+// Defmt Logging
+use defmt_rtt as _;
+
+//for the GPIO Output
+use embassy_rp::gpio::{Level, Output};
 
 /// Tell the Boot ROM about our application
-#[link_section = ".start_block"]
+#[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
 
-// Active Buzzer
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+
     let mut buzzer = Output::new(p.PIN_15, Level::Low);
 
     loop {
@@ -30,19 +34,15 @@ async fn main(_spawner: Spawner) {
     }
 }
 
-
 // Program metadata for `picotool info`.
 // This isn't needed, but it's recomended to have these minimal entries.
-#[link_section = ".bi_entries"]
+#[unsafe(link_section = ".bi_entries")]
 #[used]
 pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
-    embassy_rp::binary_info::rp_program_name!(c"Blinky Example"),
-    embassy_rp::binary_info::rp_program_description!(
-        c"This example tests the RP Pico on board LED, connected to gpio 25"
-    ),
+    embassy_rp::binary_info::rp_program_name!(c"active-beep"),
+    embassy_rp::binary_info::rp_program_description!(c"your program description"),
     embassy_rp::binary_info::rp_cargo_version!(),
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
-
 
 // End of file
